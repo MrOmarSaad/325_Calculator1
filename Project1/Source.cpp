61a7347b4948f3e985f5bdfdc301be0271c745d8
omar <omarsaad124@yahoo.com>
2020-08-20 19:49:25 +0200

    Add Division

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -9,12 +9,17 @@ int sub(int x, int y) {
 int mul(int x, int y) {
 	return x * y;
 }
+int div(int x, int y) {
+	return x / y;
+}
 void main(){
 	int res1 = add(5, 8);
 	int res2 = sub(20, 10);
 	int res3 = mul(2, 10);
+	int res4 = div(10, 2);
 	cout << res1 << endl;
 	cout << res2 << endl;
 	cout << res3 << endl;
+	cout << res4 << endl;
 	system("pause");
 }
